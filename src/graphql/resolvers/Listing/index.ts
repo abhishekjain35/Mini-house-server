36e67f88c307bcacaad821b32cca4e985dcc919a
Abhishek Jain <abhishekjai35@gmail.com>
2021-04-17 19:07:41 +0530

    Added query for listing by id

--- src/graphql/resolvers/Listing/index.ts
+++ src/graphql/resolvers/Listing/index.ts
@@ -1,10 +1,31 @@
 import { IResolvers } from "apollo-server-express";
-import { Listing } from "../../../lib/types";
+import { ObjectId } from "mongodb";
+import { Listing, Database } from "../../../lib/types";
+import { ListingArgs } from "./types";
 
 export const listingResolvers: IResolvers = {
+  Query: {
+    listing: async (
+      _root: undefined,
+      { id }: ListingArgs,
+      { db }: { db: Database }
+    ): Promise<Listing> => {
+      try {
+        const res = await db.listings.findOne({ _id: new ObjectId(id) });
+
+        if (!res) {
+          throw new Error("listing can't be found");
+        }
+
+        return res;
+      } catch (error) {
+        throw new Error(`Failed to query listing: ${error}`);
+      }
+    },
+  },
   Listing: {
     id: (listing: Listing): string => {
       return listing._id.toString();
-    }
-  }
+    },
+  },
 };
